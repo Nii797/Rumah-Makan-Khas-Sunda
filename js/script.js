fc406e9f1fbf84e5ef5bd97a2076d3e634c02c1e
Nii797 <Phanterasonpardus797@gmail.com>
2021-03-14 16:40:55 +0700

    first upload rumah makan khas sunda

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// toggle navbar
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav(){
+    navToggler.classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("open");
+}
